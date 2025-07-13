dbd1aadc399478340415caa73c3481891a7e49b3
Student <student@alx.com>
2025-07-13 22:42:16 +0300

    Update comment for DEFAULT_AUTO_FIELD in settings.py

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+# https://docs
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
